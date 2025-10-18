1d766aefbd1ca41923d005fdc5e94c4a3c00413b
Hiruka <adventuragameruluiin2017@gmail.com>
2025-10-18 12:28:14 +0300

    added //set_id to set welcome channel id

--- fembot.py
+++ fembot.py
@@ -1,7 +1,11 @@
 import discord
+from discord.ext import commands
 
 client = discord.Client(activity=discord.Game(name='Femboy Greeting Robot biep boop'), intents=discord.Intents.all())
 
+bot = commands.Bot(command_prefix='//')
+
+welcome_channel_id = 0
 
 @client.event
 async def on_ready():
@@ -10,8 +14,15 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     print(f'{member} has appeared on guild {member.guild}!')
-    channel = client.get_channel(1337132829817049150)
+    channel = client.get_channel(welcome_channel_id)
     await channel.send(f"{member.mention} Just joined, give them a nice warm welcome!")
 
+@bot.command()
+async def set_id(wc_id: int):
+    global welcome_channel_id
+    if id:
+        welcome_channel_id = wc_id
+
+bot.add_command(set_id)
 
 client.run('Token')
